3d3841d5976221294f188c7284e8f36031525a70
ali-dev-code <ali.asgr514@gmail.com>
2022-08-23 18:17:07 +0500

    uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function () {
+
+	window.onload = function () {
+		window.setTimeout(fadeout, 0);
+	};
+  function fadeout() {
+		document.querySelector(".page-loader").style.opacity = "0";
+		document.querySelector(".page-loader").style.display = "none";
+	}
+
+
+
+
+})();
